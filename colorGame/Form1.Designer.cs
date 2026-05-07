ed5aee9cee7dfb8e1c03b504522bae2cd9c7e207
bartek298 <287665@student.pwr.edu.pl>
2026-05-07 20:05:41 +0200

    Dodaj pliki projektów.

---
+++ colorGame/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace colorGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.points = new System.Windows.Forms.Label();
+            this.over = new System.Windows.Forms.PictureBox();
+            this.reset = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.over)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox2.Location = new System.Drawing.Point(244, 71);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(325, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox3.Location = new System.Drawing.Point(559, 71);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(241, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Red;
+            this.player.Location = new System.Drawing.Point(296, 376);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(195, 50);
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 22;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // points
+            // 
+            this.points.AutoSize = true;
+            this.points.BackColor = System.Drawing.Color.Transparent;
+            this.points.Font = new System.Drawing.Font("MS Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.points.Location = new System.Drawing.Point(37, 13);
+            this.points.Name = "points";
+            this.points.Size = new System.Drawing.Size(38, 40);
+            this.points.TabIndex = 4;
+            this.points.Text = "0";
+            // 
+            // over
+            // 
+            this.over.Image = ((System.Drawing.Image)(resources.GetObject("over.Image")));
+            this.over.Location = new System.Drawing.Point(72, 56);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(640, 360);
+            this.over.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.over.TabIndex = 5;
+            this.over.TabStop = false;
+            this.over.Click += new System.EventHandler(this.over_Click);
+            // 
+            // reset
+            // 
+            this.reset.AutoSize = true;
+            this.reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.reset.Location = new System.Drawing.Point(176, 69);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(377, 52);
+            this.reset.TabIndex = 6;
+            this.reset.Text = "Zagraj jeszcze raz";
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 502);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.points);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.over)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label points;
+        private System.Windows.Forms.PictureBox over;
+        private System.Windows.Forms.Label reset;
+    }
+}
+
---
+++ colorGame/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            reset.Visible = false;
+            over.Visible = false;
+        }
+        Random r = new Random();
+        Color[] colors = { Color.Blue, Color.Red, Color.Green,Color.Black };
+        int score = 0;
+        void movebar(PictureBox bars)
+        {
+            if (bars.Top <= this.Height)
+            {
+                bars.Top += 4;
+            }
+            else
+            {
+                bars.Top = 0;
+                bars.BackColor = colors[r.Next(colors.Length)];
+            }
+        }
+
+        void gameOver(PictureBox bars)
+        {
+            if (bars.Bounds.IntersectsWith(player.Bounds))
+            {
+                if (bars.BackColor != player.BackColor)
+                {
+                    timer1.Enabled = false;
+                    reset.Visible = true;
+                    over.Visible = true;
+                }
+                else
+                {
+                    score++;
+                    points.Text = score.ToString();
+                } 
+   
+            }
+        }
+        private void keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && player.Left>0)
+            {
+                player.Left += -20;
+            }
+            if (e.KeyCode == Keys.Right&& player.Right<=this.Width)
+            {
+                player.Left += +20;
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                player.BackColor = colors[r.Next(colors.Length)];
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            movebar(pictureBox1);
+            movebar(pictureBox2);
+            movebar(pictureBox3);
+            gameOver(pictureBox1);
+            gameOver(pictureBox2);
+            gameOver(pictureBox3);
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void over_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
